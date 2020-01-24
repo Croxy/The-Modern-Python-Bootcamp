0870527228138e0e165aa73500adf7083e73f14f
Ryan Cox <tr.coxy@gmail.com>
2020-01-24 09:06:29 -0600

    Organizing files/folders.

---
+++ NumbersAndMath/numsMath.py
@@ -0,0 +1,7 @@
+print(2 ** 3) #"**" indicates an exponent.
+
+print(49 ** 0.5) #Square root of 49
+
+print(10 % 3) #Modulo, kind of like division. AKA: The remainder operator.
+
+print(10//3) # // integer division, does the division, just returns an int. Always rounds down.
