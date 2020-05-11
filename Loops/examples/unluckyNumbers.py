e0ff356fb12b6b0be51d69ae9cf2e9351e7a573d
Ryan Cox <tr.coxy@gmail.com>
2020-05-11 11:25:01 -0500

    Modified unluckyNumbers example to clean up the conditional logic.

--- Loops/examples/unluckyNumbers.py
+++ Loops/examples/unluckyNumbers.py
@@ -4,13 +4,9 @@
 # for odd numbers, print "x is odd"
 
 for num in range(1, 21):
-    if num % 2 == 0:
-        if num == 4:
-            print(f"{num} is UNLUCKY!")
-        else:
-            print(f"{num} is even")
+    if num == 4 or num == 13:
+        print(f"{num} is UNLUCKY!")
+    elif num % 2 == 0:
+        print(f"{num} is even")
     elif num % 2 != 0:
-        if num == 13:
-            print(f"{num} is UNLUCKY!")
-        else:
-            print(f"{num} is odd")
+        print(f"{num} is odd")
