573f4df2f1b9e8e62de6bd40a633bba0a60ca891
Ryan Cox <tr.coxy@gmail.com>
2020-05-11 11:27:35 -0500

    Modified unluckyNumbers example to clean up the print statements.

--- Loops/examples/unluckyNumbers.py
+++ Loops/examples/unluckyNumbers.py
@@ -5,8 +5,10 @@
 
 for num in range(1, 21):
     if num == 4 or num == 13:
-        print(f"{num} is UNLUCKY!")
+        state = "unlucky"
     elif num % 2 == 0:
-        print(f"{num} is even")
+        state = "even"
     elif num % 2 != 0:
-        print(f"{num} is odd")
+        state = "odd"
+    
+    print(f"{num} is {state}")
